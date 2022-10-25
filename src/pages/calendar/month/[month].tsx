66b9626297c1f624562341219916856194f47543
enuesaa <nsrciog@gmail.com>
2022-10-26 01:32:24 +0900

    fix: api は不要なので削除する

--- src/pages/calendar/month/[month].tsx
+++ src/pages/calendar/month/[month].tsx
@@ -1,13 +1,26 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import dayjs from 'dayjs'
+
+type CalendarDate = {
+  date: string
+}
 
 const MonthPage: NextPage = () => {
   const router = useRouter()
   const { month } = router.query
+  const daysInMonth = dayjs(`${month}-01`).daysInMonth()
+  const dates: CalendarDate[] = [] 
+  for (let i = 0; i < daysInMonth; i++) {
+    dates.push({
+      date: dayjs(`${month}-01`).date(i).toISOString()
+    })
+  }
 
   return (
     <>
       {month}
+      {dates.map(v => v.date)}
     </>
   )
 }
