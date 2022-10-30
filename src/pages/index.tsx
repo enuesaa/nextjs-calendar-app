46bc949ca9cbe3da68096d14597dbc1105d37d10
enuesaa <nsrciog@gmail.com>
2022-10-30 16:20:31 +0900

    refactor: 開発しづらいのでpageを整理した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
 import type { NextPage } from 'next'
+import dayjs from 'dayjs'
+import { Date } from '@/components/calendar/Date'
+import { HtmlHead } from '@/components/common/HtmlHead'
+import { Header } from '@/components/common/Header'
+import { Main } from '@/components/common/Main'
+import { Footer } from '@/components/common/Footer'
 
+type CalendarDate = {
+  month: number;
+  day: number;
+}
 const TopPage: NextPage = () => {
-  return <>nextjs-calendar-app</>
+  const daysInMonth = dayjs(`2022-10-01`).daysInMonth()
+  const dates: CalendarDate[] = [] 
+  for (let i = 1; i <= daysInMonth; i++) {
+    dates.push({
+      month: dayjs(`2022-10-01`).date(i).month() + 1,
+      day: dayjs(`2022-10-01`).date(i).date(),
+    })
+  }
+
+  return (
+    <>
+      <HtmlHead />
+      <Header />
+      <Main>
+        2022-10
+        {dates.map(v => (<Date month={v.month} day={v.day} />))}
+      </Main>
+      <Footer />
+    </>
+  )
 }
 
 export default TopPage
