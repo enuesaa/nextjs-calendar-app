a876eb6d3827eb194c75281638e1007597792a8d
enuesaa <nsrciog@gmail.com>
2023-02-11 19:55:05 +0900

    chore(eslint): settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
   ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.cjs'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
